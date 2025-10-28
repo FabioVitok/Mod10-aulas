0ebc8ba7e2509a5d0ffd73067d045d47d1e5ad9b
F�bio Jo�o Lobo Vitoriano <37599@esjaloures.org>
2025-10-28 12:27:00 +0000

    Alterações

---
+++ ContaBancaria/ContaBancaria.java
@@ -0,0 +1,11 @@
+public class ContaBancaria
+{
+    String titular;
+    double saldo;
+    
+    public ContaBancaria(String titular, double saldo)
+    {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+   }
